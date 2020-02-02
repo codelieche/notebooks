da8bdcf1242e23967a4343dcca9c4c6e88f1e9e3
codelieche <codelieche@gmail.com>
2020-02-02 19:02:06 +0800

    update mysql56 backup.sh; add backup_local.sh

--- dockerfile/mysql/5.6/backup/backup_local.sh
+++ dockerfile/mysql/5.6/backup/backup_local.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # version 0.1
-# data 2020-02-01
-# 示例：bash backup_remote.sh codeliechedb root dbpassword root 192.168.6.123
+# data 2019-12-04
+# 示例：bash backup_remote.sh codeliechedb root dbpassword
+
 
 # 定义参数
 DatabaseName=$1          # 数据库服务的名字【加个子目录，方便区分，防止混淆】
 MYSQL_USER=$2            # 数据库用户名
 MYSQL_PASSWORD=$3        # 数据库密码
-BACKUP_HOST_USER=$4         # 远程备份的服务器用户
-BACKUP_HOST=$5           # 远程备份的服务器地址
+BACKUP_HOST=$4           # 远程备份的服务器地址
 #日期格式
 DateFormat=`date +%F`    # 日期格式：2019-12-04
 Date=`date +%y%m%d`      # 日期：20191204
 Time=`date +%T`          # 时间： 15:20:40
 DateNum=`date +%d`       # 日期天数：01
 #数据和日志路径
-BackupRootDir=/backup                                        # 备份存储的根目录【远程】
-ExecuteLogPath=/var/log/dbbackup/$DatabaseName             # 执行数据库备份操作的日志路径【本地】
-BackupFullPath=$BackupRootDir/$DatabaseName/fullbackup       # 数据库全量备份的目录【远程】
+BackupRootDir=/backup                                        # 备份存储的根目录
+ExecuteLogPath=${BackupRootDir}/$DatabaseName             # 执行数据库备份操作的日志路径
+BackupFullPath=$BackupRootDir/$DatabaseName/fullbackup       # 数据库全量备份的目录
 BackupIncrementPath=$BackupRootDir/$DatabaseName/increment   # 数据库增量备份的目录【基于全备做增备】
-MycnfPath=$BackupRootDir/$DatabaseName/etc                   # 数据库的配置文件【远程】
-FULLLSN=$BackupRootDir/full_chkpoint/$DatabaseName           # 全备的元数据信息【本地】
-INCLSN=$BackupRootDir/inc_chkpoint/$DatabaseName             # 增备的元数据信息【有子目录的-本地】
-BackupArchivePath=$BackupRootDir/$DatabaseName/archive       # 归档备份目录【全备打包目录-远程】
+MycnfPath=$BackupRootDir/$DatabaseName/etc                   # 数据库的配置文件
+FULLLSN=$BackupRootDir/full_chkpoint/$DatabaseName           # 全备的元数据信息
+INCLSN=$BackupRootDir/inc_chkpoint/$DatabaseName             # 增备的元数据信息【有子目录的】
+BackupArchivePath=$BackupRootDir/$DatabaseName/archive       # 归档备份目录【全备打包目录】
 TMPDIR=/tmp                                                  # 临时目录
 
 # innobackup参数
@@ -30,8 +30,8 @@ STREAM=xbstream
 # XTRSTR1=xtrabackup_checkpoints
 # XTRSTR2=xtrabackup_info
 
-# 检查传入的参数是否为5个
-[ $# -ne 5 ] && { echo -e "\033[41;37m [ERROR] \033[0m #xample: bash ~/backup.sh codeliechedb root password root 192.168.1.123";exit 1; }
+# 检查传入的参数是否为3个
+[ $# -ne 3 ] && { echo -e "\033[41;37m [ERROR] \033[0m #xample: bash ~/backup.sh codeliechedb root password";exit 1; }
 # 检查备份脚本日志输出目录
 id mysql > /dev/null  || { useradd mysql; }
 [ -d ${ExecuteLogPath} ] || { mkdir -p ${ExecuteLogPath};chown -R mysql. ${ExecuteLogPath}; }
@@ -65,9 +65,9 @@ softCheckFunc()
           yum update
           yum install percona-xtrabackup-24 -y
           if [ $? -eq 0 ];then
-              echo "$(date +'%F %T'):percona-xtrabackup-24安装成功";
+              echo "$(date +'%F %T'): percona-xtrabackup-24安装成功";
           else
-             echo -e "$(date +'%F %T'):\033[41;37m percona-xtrabackup-24安装失败，退出执行！\033[0m";
+             echo -e "$(date +'%F %T'): \033[41;37m percona-xtrabackup-24安装失败，退出执行！\033[0m";
              # 执行发送消息
              # "fail" "xtrabackup sofware install failure";
              exit 1;
@@ -85,9 +85,9 @@ softCheckFunc()
            apt-get update
            apt-get install percona-xtrabackup-24 -y
           if [ $? -eq 0 ];then
-              echo "$(date +'%F %T'):percona-xtrabackup-24安装成功";
+              echo "$(date +'%F %T'): percona-xtrabackup-24安装成功";
           else
-             echo -e "$(date +'%F %T'):\033[41;37m percona-xtrabackup-24安装失败，退出执行！\033[0m";
+             echo -e "$(date +'%F %T'): \033[41;37m percona-xtrabackup-24安装失败，退出执行！\033[0m";
              # 执行发送消息
              # "fail" "xtrabackup sofware install failure";
              exit 1;
@@ -99,41 +99,27 @@ softCheckFunc()
       exit 1;
     fi
 
-    echo "$(date +'%F %T'):数据库备份所需的软件包已安装"
+    echo "$(date +'%F %T'): 数据库备份所需的软件包已安装";
 }
 
 
 # 备份环境检查
 dirCheckFunc()
 {
-  echo "$(date +'%F %T'): 开始检查本地的备份元数据目录";
   # 检查存放备份信息目录
   [ -d ${FULLLSN} ] || { mkdir -p ${FULLLSN}; chown -R mysql. ${FULLLSN};  }
   [ -d ${INCLSN}  ] || { mkdir -p ${INCLSN}; chown -R mysql. ${INCLSN}; }
   [ -d ${ExecuteLogPath} ] || { mkdir -p ${ExecuteLogPath}; chown -R mysql. ${ExecuteLogPath}; }
 
   # 如果是备份数据放本地就执行下面的代码
-  # [ -d  ${BackupFullPath} ] || { mkdir -p     ${BackupFullPath}; chown -R mysql.  ${BackupFullPath}; }
-  # [ -d  ${BackupIncrementPath} ] || { mkdir -p     ${BackupIncrementPath}; chown -R mysql.  ${BackupIncrementPath}; }
-  # [ -d  ${BackupArchivePath} ] || { mkdir -p     ${BackupArchivePath}; chown -R mysql.  ${BackupArchivePath}; }
-  # [ -d  ${MycnfPath} ] || { mkdir -p     ${MycnfPath}; chown -R mysql.  ${MycnfPath}; }
+  [ -d  ${BackupFullPath} ] || { mkdir -p     ${BackupFullPath}; chown -R mysql.  ${BackupFullPath}; }
+  [ -d  ${BackupIncrementPath} ] || { mkdir -p     ${BackupIncrementPath}; chown -R mysql.  ${BackupIncrementPath}; }
+  [ -d  ${BackupArchivePath} ] || { mkdir -p     ${BackupArchivePath}; chown -R mysql.  ${BackupArchivePath}; }
+  [ -d  ${MycnfPath} ] || { mkdir -p     ${MycnfPath}; chown -R mysql.  ${MycnfPath}; }
 
   # 如果是远程备份，备份的数据需要存放在远程服务器上面就需要执行这个
-  echo "$(date +'%F %T'): 开始检查远程备份主机的的备份目录";
-  ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "[ -d  ${BackupFullPath} ] || { mkdir -p ${BackupFullPath}; }"
-  ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "[ -d  ${BackupIncrementPath} ] || { mkdir -p ${BackupIncrementPath}; }"
-  ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "[ -d  ${BackupArchivePath} ] || { mkdir -p ${BackupArchivePath}; }"
-  ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "[ -d  ${MycnfPath} ] || { mkdir -p ${MycnfPath}; }"
 
-  # 判断是否ok
-  if [ $? -eq 0 ];then
-      echo "$(date +'%F %T'): 检查本地备份元数据，远程备份主机数据目录OK";
-  else
-    echo -e "$(date +'%F %T'): \033[41;37mError: 检查本地备份元数据，远程备份主机数据目录失败；退出备份操作！\033[0m";
-    exit 1;
-  fi
 }
-
 
 
 #发送消息
@@ -147,8 +133,8 @@ sendMessageFunc()
 # 配置文件备份
 configBackupFunc()
 {
-  [ -f /etc/my.cnf ] && scp /etc/my.cnf ${BACKUP_HOST_USER}@${BACKUP_HOST}:${MycnfPath}/my.cnf.${DateFormat}
-  [ -f /etc/mysql/my.cnf ] && scp  /etc/mysql/my.cnf ${BACKUP_HOST_USER}@${BACKUP_HOST}:${MycnfPath}/mysql_my.cnf.${DateFormat}
+  [ -f /etc/my.cnf ]     && cp  /etc/my.cnf   ${MycnfPath}/my.cnf.${DateFormat}
+  [ -f /etc/mysql/my.cnf ]     && cp  /etc/mysql/my.cnf   ${MycnfPath}/mysql_my.cnf.${DateFormat}
 }
 
 
@@ -161,22 +147,21 @@ fullBackupFunc()
     mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "flush logs;";
 
     #删除上一次全备
-    ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "cd ${BackupFullPath} && ls   | xargs -n1 rm -r;" 2>/dev/null;
+    cd ${BackupFullPath} && ls   | xargs -n1 rm -r;
 
     # 执行全量备份
     # --kill-long-queries-timeout=20 可用来替换  --ftwrl-wait-threshold=60 --ftwrl-wait-timeout=120
     /usr/bin/innobackupex --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}   --ftwrl-wait-threshold=60 --ftwrl-wait-timeout=120  \
-    --stream=${STREAM} --compress  --extra-lsndir=${FULLLSN} ${TMPDIR}  | ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} \
-     "xbstream -x -C ${BackupFullPath}";
+    --stream=${STREAM} --compress  --extra-lsndir=${FULLLSN} ${TMPDIR}  | xbstream -x -C ${BackupFullPath};
 
     # 判断执行全部是否成功
     if [ $? -eq 0 ];then
         echo "$(date +'%F %T'):    ============ 全量备份成功 =============== ";
         # 全备归档
-        Size=$(ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "du -sb  $BackupFullPath" | awk -F " " '{print $1}');
-        sendMessageFunc   "执行完全备份成功！";
+        Size=$(du -sb  $BackupFullPath |awk -F " " '{print $1}');
+        sendMessageFunc   "执行全部成功！";
         # 全备文件归档
-        ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "cd ${BackupFullPath} && { tar -zcvf ${BackupArchivePath}/fullbackup_${DateFormat}.tar.gz * --remove-files; }";
+        cd ${BackupFullPath} && { tar -zcvf ${BackupArchivePath}/fullbackup_${DateFormat}.tar.gz * --remove-files; };
         [ $? -eq 0 ] || { sendMessageFunc "archived failed ${BackupFullPath}"; exit 1; };
     else
         echo "$(date +'%F %T'):    ============ 全量备份失败！！！！";
@@ -202,18 +187,18 @@ increBackupFunc()
   mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "flush logs;";
 
   # 判断增量备份的目录是否存在，不存在就创建它
-  ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "[ -d ${BackupIncrementPath}/${DateFormat} ] || { /bin/mkdir ${BackupIncrementPath}/${DateFormat}; }";
+  [ -d ${BackupIncrementPath}/${DateFormat} ] || { /bin/mkdir ${BackupIncrementPath}/${DateFormat}; };
 
   # 执行增量备份
   /usr/bin/innobackupex --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --ftwrl-wait-threshold=60 --ftwrl-wait-timeout=120   \
   --stream=${STREAM} --compress  --extra-lsndir=${INCLSN}/${DateFormat} ${TMPDIR} \
-  --incremental  --incremental-basedir=${FULLLSN} | ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} "xbstream -x -C ${BackupIncrementPath}/${DateFormat}";
+  --incremental  --incremental-basedir=${FULLLSN} | xbstream -x -C ${BackupIncrementPath}/${DateFormat};
 
   # 判断执行是否成功
   if [ $? -eq 0 ];then
       echo "$(date +'%F %T'):    ============ 增量备份成功 ===============";
 
-      Size=$(ssh -o stricthostkeychecking=no ${BACKUP_HOST_USER}@${BACKUP_HOST} -C "du -sb  $BackupIncrementPath" | awk -F " " '{print $1}');
+      Size=$(du -sb  $BackupIncrementPath |awk -F " " '{print $1}');
       echo "${now}:    ============ 增量备份文件大小：$Size ===============";
       #		sendMessageFunc "success" "success"  "$Size" "increment"  "$BackupIncrementPath"
   else
@@ -224,6 +209,7 @@ increBackupFunc()
       exit 1
   fi
 }
+
 
 # 执行备份主函数
 mainFunc()
@@ -253,7 +239,7 @@ mainFunc()
       # 检查出错
       echo -e "\033[41;37m[WARNING] \033[0m  Please Check The Env $IPLIST ";
     fi
-    echo "$(date +'%F %T'):Done"
+    echo "$(date +'%F %T'): Done"
 }
 
 echo "$(date +'%F %T'): 开始执行";
